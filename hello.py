d0cb13f12c3d231f33678ee45a71ef98a1f361f2
Youssef Bouraha <bouraha.youssaf@gmail.com>
2026-01-08 13:43:49 +0100

    data pipiline is completed

--- hello.py
+++ hello.py
@@ -1,2 +1,14 @@
 
 print('hello youssef ')
+import os
+from pathlib import Path
+
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+print( 'ths is the path folder' ,PROJECT_ROOT)
+
+
+from pathlib import Path
+
+PROJECT_ROOT = Path.cwd()   # ← THIS is the key
+print( 'ths is the path folder' ,PROJECT_ROOT)
--- main.py
+++ main.py
@@ -3,33 +3,22 @@
 # ===============================
 
 import pandas as pd
-from src.data_preprocessing import prepare_df, train_test_split
-from src.feature_engineering import date_features, lags, exg_features
-from utilsforecast.feature_engineering import pipeline  # applies features to data
+import os
+from pathlib import Path
 
 
-# -------------------------------
-# 1. Load and preprocess data
-# -------------------------------
-df = pd.read_csv('data/PJME_hourly.csv')  # raw CSV
-data = prepare_df(df)                      # clean, rename, add unique_id
-print("Preprocessed data sample:")
-print(data.head())
 
-# -------------------------------
-# 2. Split into train and test
-# -------------------------------
-split_date = '2017-01-01'
-train, test = train_test_split(data, split_date)
+
+PROJECT_ROOT = Path.cwd() 
+
+
+train_path = os.path.join(PROJECT_ROOT, 'data', 'preprocessed', 'train.csv')  
+test_path = os.path.join(PROJECT_ROOT, 'data', 'preprocessed', 'test.csv')  
+
+train = pd.read_csv(train_path)
+test = pd.read_csv(test_path)
+
+print(f'✅ Train and test data are successfully loaded and ready for use.')
 print(f'Train shape: {train.shape}, Test shape: {test.shape}')
-
-# -------------------------------
-# 3. Apply exogenous features
-# -------------------------------
-# exg_features() returns list of feature functions (Fourier, etc.)
-exg_train, exg_test = pipeline(train, h=len(test), freq='h', features=exg_features())
-print(f'Exogenous train shape: {exg_train.shape}, Exogenous test shape: {exg_test.shape}')
-
-# -------------------------------
-# Now exg_train and exg_test can be used in MLForecast
-# -------------------------------
+print("Train data sample:")
+print(train.head())
